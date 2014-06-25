5a7520398b14e684bbf8c5a4deff2aa413c61537
lucbettaieb <luc.bettaieb@gmail.com>
2014-06-25 16:29:44 -0500

    Gettin' the ol' ROS workspace goin'.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/luc/Developer/flexcraft/src
+# Build directory: /home/luc/Developer/flexcraft/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
