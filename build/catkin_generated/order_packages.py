c9f439481960a20d9565453fcbfb3d259d310a2a
fcs_basestation <fcs_basestation@chandra.(none)>
2014-07-07 12:30:19 -0500

    removed build folder

--- build/catkin_generated/order_packages.py
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/fcs_basestation/catkin_ws/src/flexcraft/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
