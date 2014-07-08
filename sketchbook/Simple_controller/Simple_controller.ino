af2fe1ee85c83d28dc1e8f7436704bee1ee87db4
viki <viki@c3po.(none)>
2014-07-08 08:25:40 -0700

    "Actually updating the simple controller"

--- sketchbook/Simple_controller/Simple_controller.ino
+++ sketchbook/Simple_controller/Simple_controller.ino
@@ -1,66 +1,84 @@
 #include <ArduinoHardware.h>
 #include <ros.h>
 #include "flexcraft_msgs/deltaV2D.h"
+
 ros::NodeHandle nh;
-
-std_msgs::String str_msg;
-ros::Publisher joystick = nhadvertise<flexcraft_msgs::deltaV2D>("mid_cmd",1);
-
-ROS_INFO("Good Morning Doctor Chandra");
 
 int x_plus;
 int x_minus;
 int y_minus;
 int y_plus;
+int z_plus;
+int z_minus;
 
+flexcraft_msgs::deltaV2D cmd_msg;
+
+ros::Publisher controller_pub("cmd_msg", &cmd_msg);
 
 void setup(){
+  Serial.println("Initializing...");
+  
+  nh.advertise(controller_pub);
+  
   nh.initNode();
+  
+  Serial.println("Initialized!");
+ 
+    cmd_msg.deltaVX = 0;
+    cmd_msg.deltaVY = 0;
+    cmd_msg.alpha = 0;
 }
 
 void loop(){
     // read the input on analog pin 0:
-   x_plus = analogRead(A0);
-   x_minus = analogRead(A3);
-   y_minus = analogRead(A2);
-   y_plus = analogRead(A1);
-  
 
-//Assign boolean values
-  if(x_plus > 900){
-    //up = true;
-    
+   x_plus = analogRead(A3);
+   x_minus = analogRead(A1);
+   y_minus = analogRead(A2);
+   y_plus = analogRead(A0);
+   z_plus = analogRead(A5);
+   z_minus = analogRead(A4);  
+
+//Check direction commands, send them
+//If we're going forward or backward, do that; else, don't do that.
+  
+  if(x_plus > 0){
+    cmd_msg.deltaVX = 1.0;
+    //Serial.println("ONWARD");
+  }
+  else if(x_minus > 0){
+    cmd_msg.deltaVX = -1.0;
+    //Serial.println("RETREAT");  
   }
   else{
-    //up = false;
+    cmd_msg.deltaVX = 0;
   }
   
-  if(x_minus > 900){
+  //Left/right control
+  if(y_minus > 0){
+    cmd_msg.deltaVY = -1.0;
     //down = true;
   }
-  else{
-    //down = false;
-  }
-  
-  if(y_plus >900){
-    //left = true;
+  else if(y_plus > 0){
+    cmd_msg.deltaVY =  1.0; 
   }
   else{
-    //left = false;
+    cmd_msg.deltaVY = 0.0;
   }
-  
-  if(y_minus > 900){
-   // right = true;
+  //rotation
+  if(z_plus > 0){
+    cmd_msg.alpha = 1.0;
+  }
+  else if(z_minus > 0){
+    cmd_msg.alpha = -1.0;
   }
   else{
-    //right = false;
+    cmd_msg.alpha = 0.0;
   }
   
-//  Serial.print("Forward: "); Serial.println(forward);
-//  Serial.print("Backward: "); Serial.println(backward);
-//  Serial.print("Left: "); Serial.println(left);
-//  Serial.print("Right: "); Serial.println(right);
+  controller_pub.publish(&cmd_msg);
+
   nh.spinOnce();
-  delay(1);        // delay in between reads for stability
+  delay(10);        // delay in between reads for stabil  ity
 }
 
