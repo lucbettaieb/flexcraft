29e64594d4328d4b24eb6beb062bb59d409caf7f
fcs_main <fcs_main@HAL9000.(none)>
2014-08-01 15:57:40 -0500

    fixed kinect.launch

--- flexcraft_onboard_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#This file launches the FlexCraft onboard code, allowing for the user to use a mouse instead of requring a keyboard.
-
-source /opt/ros/hydro/setup.bash
-source /home/fcs_main/catkin_ws/src/flexcraft/devel/setup.bash
-
-roslaunch fcs_launch start_all.launch
