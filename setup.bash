123fc413ac227b7a12bf0c4a26f741735366f927
fcs_main <fcs_main@HAL9000.(none)>
2014-08-04 12:03:34 -0500

    creating single command setup file, including desktop creating scripts

---
+++ setup.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#this file fully sets up flexcraft code on an ubuntu 12.04 machine
+#it install ros hydro and then installs the code
+
+#installs ros hydro following the instructions from wiki.ros.org/hydro/Installation/Ubuntu
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+
+sudo apt-get update
+
+sudo apt-get install ros-hydro-desktop-full
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
+
+#set up flexcraft code (assumes code already pulled as this file is in repository)
+catkin_make
+
+#sources flexcraft file so ros knows of this code
+here=$PWD
+echo "source $here/devel/setup.bash" >> ~/.bashrc
+
+#makes this terminal reset
+source ~/.bashrc
+
+#install some file dependencies
+sudo apt-get install ros-hydro-camera-info-manager-py
+rosdep install spacenav_node
+
+# remakes the system so it is good to go.  -j1 does it sequentially so not dependency errors
+catkin_make -j1
+
+# create desktop files
+./create_desktop.bash
--- startup/scripts/flexcraft_teleop_start.sh
+++ startup/scripts/flexcraft_teleop_start.sh
@@ -2,9 +2,18 @@
 
 #This file launches the FlexCraft onboard code, allowing for the user to use a mouse instead of requring a keyboard.
 
+#These variables allows input to be done in desktop application.  First variable is for launching human input device spacenav(3d mouse) and second is keyboard. 
+spacenav=false
+keyboard=false
+spacenav=$1
+keyboard=$2
+
+#This sets up the ros environment
 source /opt/ros/hydro/setup.bash
 source ~/catkin_ws/src/flexcraft/devel/setup.bash
 
-export ROS_URI_MASTER=chandra
+#This sets the other computer as master
+export ROS_MASTER_URI=http://chandra:11311
 
-roslaunch fcs_launch start_teleop.launch
+#This actually launches code
+roslaunch fcs_launch start_teleop.launch spacenav:=$spacenav keyboard:=$keyboard
